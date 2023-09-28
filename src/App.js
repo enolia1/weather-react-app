24090b343c0c3fe474f03ba355011ef2c9a4beba
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-28 15:05:27 +0200

    fixed html added j.s and c.ss removed images installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by <a href="">Enolia Ritabile Monareng</a> and
+          is{" "}
+          <a href="https://github.com/enolia1/weather-react-app">
+            open-sourced on GitHub
+          </a>{" "}
+          hosted in{" "}
+          <a href="https://glittering-gingersnap-ce8ae9.netlify.app/">
+            Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="Search"
+              placeholder="Enter a City..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="Submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>wednessday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+              alt="Mostly Cloudy"
+            />
+            <span className="temperature">6</span>
+            <span className="unit"> °C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 4%</li>
+            <li>Humidity: 84%</li>
+            <li>Wind: 13 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
