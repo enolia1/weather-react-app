6d84e7c56f582f8f6b53298029e424a0cf38869c
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-28 15:24:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ export default function App() {
       <div className="container">
         <Weather />
         <footer>
-          This project was coded by <a href="">Enolia Ritabile Monareng</a> and
-          is{" "}
-          <a href="https://github.com/enolia1/weather-react-app">
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/86838-enolia-ritabile-monareng"
+            target="_blank"
+          >
+            Enolia Ritabile Monareng
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/enolia1/weather-react-app"
+            target="_blank"
+          >
             open-sourced on GitHub
           </a>{" "}
           hosted in{" "}
-          <a href="https://glittering-gingersnap-ce8ae9.netlify.app/">
+          <a
+            href="https://app.netlify.com/sites/moonlit-sawine-3d269d/deploys"
+            target="_blank"
+          >
             Netlify
           </a>
         </footer>
